05b41773ea6d47d1a09b5cd7bcd13cd94b5c7eb8
Jad Ismail <jadismail665@gmail.com>
2022-07-20 12:48:04 +0300

    fixing the app

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.post("/delete", (req, res) => {
         }
     });
 });
-
-app.listen(3000, () => {
-    console.log("server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("server running on port " + PORT);
 });
